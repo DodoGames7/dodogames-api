3161fc566ae43c23be6c2397560f153a9cd7ce87
Glitch (glitch-hello-node) <none>
2022-06-26 19:24:18 +0000

    🌒💂 Checkpoint ./routes/fun/fact.js:26877849/86

--- routes/fun/fact.js
+++ routes/fun/fact.js
@@ -1,7 +1,5 @@
 module.exports = {
   path: '/fun/fact',
-  code: `$send[200;json;{
-  fact: '$getData[data.fact]'
-  }]' 
+  code: `$send[$getData[data.fact]] 
 $request[https://api.popcat.xyz/fact]`
 }
