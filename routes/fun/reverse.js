2a23473cd2b25e6aabedea443476e45c3043d301
Glitch (glitch-hello-node) <none>
2022-06-27 10:44:55 +0000

    🐔😋 Checkpoint ./routes/fun/reverse.js:26877849/17

--- routes/fun/reverse.js
+++ routes/fun/reverse.js
@@ -3,7 +3,7 @@ module.exports = {
   code: `$send[200;json;{
   text: '$getData[data.text]'
   }]
-  $request[https://api.miduwu.ga/json/reverse?text=hi]
+  $request[https://api.miduwu.ga/json/reverse?text=$getQuery[text]]
   $if[$getQuery[text]==undefined;400;{
 error: 'Missing query parameter called text.'
 }]`
