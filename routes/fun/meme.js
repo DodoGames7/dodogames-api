4891d374522fe41fb2e6d29e1473f3ec67948a81
Glitch (glitch-hello-node) <none>
2022-06-27 13:53:42 +0000

    👩‍❤️‍💋‍👩☄️ Checkpoint ./routes/fun/meme.js:26877849/104

--- routes/fun/meme.js
+++ routes/fun/meme.js
@@ -2,11 +2,12 @@ module.exports = {
   path: '/fun/meme',
   code: `$send[200;json;{
   title: '$getData[data.title]',
-  image: '$getData[post.data.url]'
+  image: '$getData[data.url]'
  
   $request[https://www.reddit.com/r/memes/random/.json;{
-  method: 'POST',
-  }]
+  method: 'GET',
+  data: '$jsEval[const data = json[0].data.children[0].data;]'
+  };Math.random().toString()]
   }]
 ]
   `
