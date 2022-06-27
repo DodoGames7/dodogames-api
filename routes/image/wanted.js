6b78aa095bbe3667ac1e4a668bbd4d5565a6fb89
Glitch (glitch-hello-node) <none>
2022-06-27 13:06:47 +0000

    🗽🏀 Checkpoint ./routes/image/wanted.js:26877849/111

--- routes/image/wanted.js
+++ routes/image/wanted.js
@@ -2,10 +2,10 @@ module.exports = {
     path: '/image/wanted',
     code: `
 $send[200;canvas;$default]
+$drawImage[avatar;0;0;64;64]
 $drawImage[filter;0;0;512;512]
-$drawImage[avatar;0;0;400;400]
 $createCanvas[512;512]
-$loadImage[filter;url;$replaceText[https://cdn.discordapp.com/attachments/647127947144069120/990960395458064414/cachedImage.png;$getQuery[image]]]
+$loadImage[filter;url;https://cdn.discordapp.com/attachments/647127947144069120/990960395458064414/cachedImage.png]
 $if[$isImage[avatar]==false;400;{
 error: 'Invalid image provided.'
 }]
