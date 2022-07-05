2241dfc5ad42a420dc6d1234d75949ea2ec01921
Glitch (glitch-hello-node) <none>
2022-07-05 22:54:23 +0000

    🎉🏖 Checkpoint ./routes/fun/reverse.js:26877849/2 ./package.json:26877849/42

--- routes/fun/reverse.js
+++ routes/fun/reverse.js
@@ -2,9 +2,7 @@ module.exports = {
   path: "/fun/reverse",
   code: `$send[200;json;{
   text: '$jsEval[String.prototype.reverse = function () {
-    return this.split('').reverse().join('$getQuery[text]');
-}
-]'
+    return this.split('').reverse().join('$getQuery[text]');}]'
 }]
   $if[$getQuery[text]==undefined;400;{
 error: 'Missing query parameter called text.'
