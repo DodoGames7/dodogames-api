bbae9e86092acb5213f7d500a713d98be05fc833
Glitch (glitch-hello-node) <none>
2022-07-07 00:33:19 +0000

    👀🌶 Checkpoint ./routes/image/drake.js:26877849/443

---
+++ routes/image/drake.js
@@ -0,0 +1,10 @@
+module.exports = {
+  path:"/image/drake", 
+  code:`$send[200;canvas;$default]
+$drawText[text;20;20;450;400]
+$drawImage[lol;0;0;485;564]
+$loadImage[lol;link;https://imgflip.com/s/meme/Drake-Hotline-Bling.jpg]
+
+
+$createCanvas[485;564]`
+}
