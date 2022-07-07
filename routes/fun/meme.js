f0027b15e293f14ce636476609c8d2615c1a2f34
Glitch (glitch-hello-node) <none>
2022-07-07 00:44:24 +0000

    👂⚽️ Checkpoint ./routes/image/drake.js:26877849/27 ./routes/fun/meme.js:26877849/231

--- routes/fun/meme.js
+++ routes/fun/meme.js
@@ -4,11 +4,8 @@ module.exports = {
   title: '$getData[data.title]',
   image: '$getData[data.url]'
  
-  $request[https://www.reddit.com/r/memes/random/.json;{
-  method: 'GET',
-  data: '$jsEval[const data = json[0].data.children[0].data;]'
-  };Math.random().toString()]
   }]
+  $request[https://www.reddit.com/r/memes/random/.json]
 ]
   `
 }
--- routes/image/drake.js
+++ routes/image/drake.js
@@ -1,7 +1,7 @@
 module.exports = {
   path:"/image/drake", 
   code:`$send[200;canvas;$default]
-$drawText[text;20;20;450;400]
+$drawText[$getQuery[text];50;50;450;400]
 $drawImage[lol;0;0;485;564]
 $loadImage[lol;link;https://imgflip.com/s/meme/Drake-Hotline-Bling.jpg]
 
