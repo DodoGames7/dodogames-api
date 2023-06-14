7f61cc1ccd1ef3e49cbd88d03b7a4a99c72e709d
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-14 11:48:04 +0300

    more topics in `/randomtopic` endpoint

--- routes/fun/randomtopic.js
+++ routes/fun/randomtopic.js
@@ -1,6 +1,6 @@
 module.exports = {
     path: '/randomtopic',
-    code: `$var[topic;$random[What did you eat today?;What are you currently worried about?;What game have you spent the most hours playing?;What game have you spent the most hours playing?;What movie can you watch over and over without ever getting tired of?;What are you interested in that most people aren't?;How do you judge a person?;What do you think you are much better at than you actually are?;Do you enjoy spicy food?;Where are some unusual places you've been?;What movie quotes do you use on a regular basis?;Did you go out today?]]
+    code: `$var[topic;$random[What did you eat today?;What are you currently worried about?;What game have you spent the most hours playing?;What game have you spent the most hours playing?;What movie can you watch over and over without ever getting tired of?;What are you interested in that most people aren't?;How do you judge a person?;What do you think you are much better at than you actually are?;Do you enjoy spicy food?;Where are some unusual places you've been?;What movie quotes do you use on a regular basis?;Did you go out today?;What was the last photo you took?;What movie quotes do you use on a regular basis?;Which restaurant is the best in your opinion?;What languages do you speak?]]
     $send[200;json;{
     topic: '$get[topic]'
     }]
