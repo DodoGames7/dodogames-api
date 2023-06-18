fca23c1e641a1f9ecd62e234767c1c9dbbb17efc
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-06-18 17:07:23 +0300

    fixed `/color` endpoint

--- index.js
+++ index.js
@@ -8,23 +8,6 @@ const api = new API({
   }
 })
 
-api.routes.add({
-    path: '/color',
-    code: `
-    $ignore[Check docs to see how does functions work]
-    $send[200;canvas;$default]
-    $drawRect[0;0;512;512]
-    $color[$getQuery[hex]]
-    $createCanvas[512;512]
-    $if[$isValidHex[$getQuery[hex]]==false;400;{
-        error: "Invalid hex code provided."
-    }]
-    $if[$getQuery[hex]==undefined;400;{
-        error: "Missing 'hex' parameter."
-    }]
-    `
-})
-
 
 // Lets load the handler...
 api.routes.load('./routes').then(() => {
