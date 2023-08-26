4d882a295fe15a72de9a8d0a90d35467c0eaf5bc
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-08-26 20:27:38 +0300

    Fix owoify argument error

--- routes/text/owoify.js
+++ routes/text/owoify.js
@@ -2,12 +2,13 @@
 module.exports = {
     path: '/text/owoify',
     code: `
+    $if[$getQuery[text]==undefined;400;{
+error: 'Missing query parameter called text.'
+}]
+
 $send[200;json;{
 error: false,
 owoified: '$replaceRegexp[$replaceRegexp[$getQuery[text];/r|l/;g;w];/R|L/;g;W]'
-}]
-$if[$getQuery[text]==undefined;400;{
-error: 'Missing query parameter called text.'
 }]
 `
 }
